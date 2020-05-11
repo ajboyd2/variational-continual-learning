2fe6a439d4eadf6c4a4865b04f202bd6869a6272
Alex James Boyd <alexjb@datalab-gpu1.ics.uci.edu>
2020-05-11 04:29:23 -0700

    Beam search is operational now.

--- improved_ddm/alg/vcl.py
+++ improved_ddm/alg/vcl.py
@@ -4,6 +4,7 @@ from cla_models_multihead import MFVI_NN
 from copy import deepcopy
 import time
 import os
+import pickle
 
 ide_func = lambda x: np.float32(x)
 log_func = lambda x: np.float32(np.log(x))
@@ -64,6 +65,14 @@ class Hypothesis:
         self.s_t = s_t
         self.diffusion = diffusion
         self.task_id = task_id
+        self.prev_results = prev_results
+
+        print("\n----------------------------------------------------------------")
+        print(f"Hypothesis created for {self.get_weight_save_dir()}")
+        print(f"Previous weights located at {self.prev_weights_path}")
+        print(f"Jump variable value of {s_t} with diffusion of {diffusion}")
+        print("----------------------------------------------------------------\n")
+
 
     def get_weights(self, no_lower_weights, no_upper_weights):
         ws = WeightsStorage(
@@ -74,8 +83,10 @@ class Hypothesis:
         )
 
         if self.prev_weights_path is None:
+            print("Model loaded with initial priors.")
             return ws
         else:
+            print(f"Model loaded with prior from {self.prev_weights_path}")
             checkpoint = np.load(self.prev_weights_path)
             ws.store_weights(
                 post_l_mv=checkpoint["lower"], 
@@ -83,7 +94,10 @@ class Hypothesis:
             )
 
             if self.s_t == 1:
+                print(f"Model priors will be broadened as the jump variable is {self.s_t}")
                 ws.broaden_weights(self.diffusion)
+            else:
+                print(f"Model priors will not be broadened as the jump variable is {self.s_t}")
 
             return ws
 
@@ -93,7 +107,7 @@ class Hypothesis:
 class RunResults:
     def __init__(self, hypothesis, elbo, bias, test_metrics):
         self.hypothesis = hypothesis
-        self.prev_weights_path = hypothesis.prev_weights_path
+        self.weights_path = hypothesis.get_weight_save_dir()
         self.save_path = hypothesis.save_path
         self.s_t = hypothesis.s_t
         self.prev_results = hypothesis.prev_results
@@ -101,7 +115,7 @@ class RunResults:
         self.bias = bias
         self.test_metrics = test_metrics
         if self.prev_results is not None:
-            self.prev_results.register_child(self, s_t)
+            self.prev_results.register_child(self, self.s_t)
 
         self.child_s0 = None
         self.child_s1 = None
@@ -125,7 +139,7 @@ class RunResults:
 
         z = self.child_s1.elbo - self.child_s0.elbo + self.bias
         # log q(s_t=1) = log (sigmoid(z)) = log (1 / (1 + exp(-z)) = -log(1+exp(-z))
-        self.child_s1.single_log_prob = -np.log1p(np.exp(-1 * z)))
+        self.child_s1.single_log_prob = -np.log1p(np.exp(-1 * z))
         # log q(s_t=0) = log (1 - q(s_t=1)) = log(1 - sigmoid(z)) = log(sigmoid(-z)) = -log(1+exp(z))
         self.child_s0.single_log_prob = -np.log1p(np.exp(z))
 
@@ -135,7 +149,7 @@ class RunResults:
 
 # Stores the current beams being considered and holds locations of weights.
 class BeamSearchHistory:
-    def __init__(self, directory, max_beams=2, diffusion=1.0, jump_bias=0.0):
+    def __init__(self, directory, max_beams=2, diffusion=1.0, jump_bias=0.0, max_depth=10):
         self.directory = directory
         self.max_beams = max_beams
         self.diffusion = diffusion
@@ -143,12 +157,22 @@ class BeamSearchHistory:
         self.root = None
         self.current_beams = []
         self.depth = 0
+        self.max_depth = max_depth
+
+        print("\n================================================================")
+        print(f"Performing variational beam search with a beam size of {max_beams},")
+        print(f"a diffusion constant of {diffusion}, jump bias of {jump_bias},")
+        print(f"over {max_depth} different tasks. Will save results to {directory}.")
+        print("----------------------------------------------------------------\n")
 
     def get_new_hypotheses(self):
+        if self.depth == self.max_depth:
+            return []
+        
         if self.root is None:
             return [Hypothesis(
                 prev_weights_path=None, 
-                save_path=f"{self.directory.rstrip("/")}/beam_0", 
+                save_path=f"{self.directory.rstrip('/')}/beam_0", 
                 s_t=0, 
                 diffusion=self.diffusion, 
                 task_id=0, 
@@ -163,7 +187,7 @@ class BeamSearchHistory:
                     options = [0, 1]
                 for s_t in options:
                     hypotheses.append(Hypothesis(
-                        prev_weights_path=beam.save_path,
+                        prev_weights_path=beam.weights_path,
                         save_path=f"{beam.save_path}{s_t}", 
                         s_t=s_t, 
                         diffusion=self.diffusion, 
@@ -185,22 +209,25 @@ class BeamSearchHistory:
             assert(len(result_nodes) == 1)
             self.root = result_nodes[0]
         
-        self.beams = result_nodes
+        print(f"{len(result_nodes)} have been processed")
+        self.current_beams = result_nodes
         self.depth += 1
     
     def prune_beams(self):
-        sorted_beams = sorted(self.beams, key=lambda x: -x.total_log_prob)
-        self.beams = sorted_beams[:self.max_beams]
+        sorted_beams = sorted(self.current_beams, key=lambda x: -x.total_log_prob)
+        print(f"Sorted weights of beams are: {[b.total_log_prob for b in sorted_beams]}")
+        self.current_beams = sorted_beams[:self.max_beams]
+        print(f"Beams remaining after truncation: {len(self.current_beams)}")
         
     
 # Factory function to return a beam search object
 def get_beam_search_history(directory, **kwargs):
-    beam_path = f"{directory.rstrip("/")}/beams.pkl"
+    beam_path = f"{directory.rstrip('/')}/beam_history.pkl"
     if os.path.isfile(beam_path):
         # TODO: Check if model hyperparameters match for the current run and the history being loaded
         return pickle.load(open(beam_path, "rb"))
     else:
-        return BeamSearchHistory(file_path=beam_path, **kwargs)
+        return BeamSearchHistory(directory=directory, **kwargs)
 
 # Initialise model weights before training on new data, using small random means and small variances
 def initialise_weights(weights):
@@ -255,105 +282,138 @@ def run_vcl_shared(hidden_size, no_epochs, data_gen, coreset_method, coreset_siz
     no_lower_weights = model.lower_net.no_weights
     no_upper_weights = [net.no_weights for net in model.upper_nets]
 
+    bsh = get_beam_search_history(
+        directory=path, 
+        max_beams=beam_size, 
+        diffusion=diffusion, 
+        jump_bias=jump_bias,
+        max_depth=no_tasks,
+    )
+
     # Set up model weights at initial prior
-    weights_storage = WeightsStorage(no_lower_weights, no_upper_weights, prior_mean=0.0, prior_var=1.0)
+    #weights_storage = WeightsStorage(no_lower_weights, no_upper_weights, prior_mean=0.0, prior_var=1.0)
 
-    for task_id in range(no_tasks):
-        # tf init model
-        model.init_session(task_id, learning_rate, training_classes[task_id])
+    hypotheses = bsh.get_new_hypotheses()
+    #for task_id in range(no_tasks):
+    while len(hypotheses) > 0:
+        task_id = bsh.depth  # at start will be 0
+        results = []
+        for hypothesis in hypotheses:
+            result = {}
+            weights_storage = hypothesis.get_weights(no_lower_weights, no_upper_weights)
+            # tf init model
+            model.init_session(task_id, learning_rate, training_classes[task_id])
 
-        # Get data
-        x_train, y_train = x_trainsets[task_id], y_trainsets[task_id]
+            # Get data
+            x_train, y_train = x_trainsets[task_id], y_trainsets[task_id]
 
-        # Set batch size
-        bsize = x_train.shape[0] if (batch_size is None) else batch_size
+            # Set batch size
+            bsize = x_train.shape[0] if (batch_size is None) else batch_size
 
-        # Select coreset if needed
-        if coreset_size > 0:
-            x_coresets, y_coresets, x_train, y_train = coreset_method(
-                x_coresets, y_coresets, x_train, y_train, coreset_size)
+            # Select coreset if needed
+            if coreset_size > 0:
+                x_coresets, y_coresets, x_train, y_train = coreset_method(
+                    x_coresets, y_coresets, x_train, y_train, coreset_size)
 
-        # Prior of weights is previous posterior (or, if first task, already in weights_storage)
-        lower_weights_prior, upper_weights_prior = weights_storage.return_weights()
+            # Prior of weights is previous posterior (or, if first task, already in weights_storage)
+            lower_weights_prior, upper_weights_prior = weights_storage.return_weights()
 
-        # Initialise using random means + small variances
-        lower_weights = initialise_weights(lower_weights_prior)
-        upper_weights = deepcopy(upper_weights_prior)
-        for class_id in training_classes[task_id]:
-            upper_weights[class_id] = deepcopy(initialise_weights(upper_weights_prior[class_id]))
+            # Initialise using random means + small variances
+            lower_weights = initialise_weights(lower_weights_prior)
+            upper_weights = deepcopy(upper_weights_prior)
+            for class_id in training_classes[task_id]:
+                upper_weights[class_id] = deepcopy(initialise_weights(upper_weights_prior[class_id]))
 
-        # Assign initial weights to the model
-        model.assign_weights(list(range(no_heads)), lower_weights, upper_weights)
+            # Assign initial weights to the model
+            model.assign_weights(list(range(no_heads)), lower_weights, upper_weights)
 
-        # Train on non-coreset data
-        model.reset_optimiser()
+            # Train on non-coreset data
+            model.reset_optimiser()
 
-        start_time = time.time()
-        _, _ = model.train(x_train, y_train, task_id, lower_weights_prior, upper_weights_prior, no_epochs, bsize)
-        end_time = time.time()
-        print('Time taken to train (s):', end_time - start_time)
+            start_time = time.time()
+            costs, lik_costs = model.train(x_train, y_train, task_id, lower_weights_prior, upper_weights_prior, no_epochs, bsize)
+            result["elbo"] = costs[-1]  # this is the elbo from the last epoch
+            end_time = time.time()
+            print('Time taken to train (s):', end_time - start_time)
 
-        # Get weights from model, and store in weights_storage
-        lower_weights, upper_weights = model.get_weights(list(range(no_heads)))
-        weights_storage.store_weights(lower_weights, upper_weights)
+            # Get weights from model, and store in weights_storage
+            lower_weights, upper_weights = model.get_weights(list(range(no_heads)))
+            weights_storage.store_weights(lower_weights, upper_weights)
 
-        # Save model weights after training on non-coreset data
-        if store_weights:
-            np.savez(path + 'weights_%d.npz' % task_id, lower=lower_weights, upper=upper_weights,
-                     classes=data_gen.classes,
-                     MNISTdigits=data_gen.sets, class_index_conversion=data_gen.class_list)
+            # Save model weights after training on non-coreset data
+            if store_weights:
+                np.savez(
+                    hypothesis.get_weight_save_dir(),
+                    #path + 'weights_%d.npz' % task_id, 
+                    lower=lower_weights, 
+                    upper=upper_weights,
+                    classes=data_gen.classes,
+                    MNISTdigits=data_gen.sets, 
+                    class_index_conversion=data_gen.class_list,
+                )
 
-        model.close_session()
+            model.close_session()
 
-        # Train on coreset data, then calculate test accuracy
-        if multi_head:
-            acc = np.zeros(no_tasks)
-            for test_task_id in range(task_id+1):
+            # Train on coreset data, then calculate test accuracy
+            if multi_head:
+                acc = np.zeros(no_tasks)
+                for test_task_id in range(task_id+1):
+                    # Initialise session, and load weights into model
+                    model.init_session(test_task_id, learning_rate, training_classes[test_task_id])
+                    lower_weights, upper_weights = weights_storage.return_weights()
+                    model.assign_weights(list(range(no_heads)), lower_weights, upper_weights)
+                    if len(x_coresets) > 0:
+                        print('Training on coreset data...')
+                        # Train on each task's coreset data just before testing on that task
+                        x_train_coreset, y_train_coreset = x_coresets[test_task_id], y_coresets[test_task_id]
+                        bsize = x_train_coreset.shape[0] if (batch_size is None) else batch_size
+                        model.reset_optimiser()
+                        _, _ = model.train(x_train_coreset, y_train_coreset, test_task_id,
+                                        lower_weights, upper_weights, no_epochs, bsize)
+
+                    # Test-time: Calculate test accuracy
+                    acc_interm = utils.get_scores_output_pred(model, x_testsets, y_testsets, test_classes,
+                                                    task_idx=[test_task_id], multi_head=multi_head)
+                    acc[test_task_id] = acc_interm[0]
+
+                    model.close_session()
+
+            else:
+                acc = np.zeros(no_tasks)
                 # Initialise session, and load weights into model
-                model.init_session(test_task_id, learning_rate, training_classes[test_task_id])
+                model.init_session(task_id, learning_rate, training_classes[task_id])
                 lower_weights, upper_weights = weights_storage.return_weights()
                 model.assign_weights(list(range(no_heads)), lower_weights, upper_weights)
                 if len(x_coresets) > 0:
                     print('Training on coreset data...')
-                    # Train on each task's coreset data just before testing on that task
-                    x_train_coreset, y_train_coreset = x_coresets[test_task_id], y_coresets[test_task_id]
+                    x_train_coreset, y_train_coreset = utils.merge_coresets(x_coresets, y_coresets)
                     bsize = x_train_coreset.shape[0] if (batch_size is None) else batch_size
-                    model.reset_optimiser()
-                    _, _ = model.train(x_train_coreset, y_train_coreset, test_task_id,
-                                       lower_weights, upper_weights, no_epochs, bsize)
+                    _, _ = model.train(x_train_coreset, y_train_coreset, task_id,
+                                    lower_weights, upper_weights, no_epochs, bsize)
 
                 # Test-time: Calculate test accuracy
                 acc_interm = utils.get_scores_output_pred(model, x_testsets, y_testsets, test_classes,
-                                                   task_idx=[test_task_id], multi_head=multi_head)
-                acc[test_task_id] = acc_interm[0]
+                                                task_idx=list(range(task_id+1)), multi_head=multi_head)
+                acc[:task_id+1] = acc_interm
 
                 model.close_session()
 
-        else:
-            acc = np.zeros(no_tasks)
-            # Initialise session, and load weights into model
-            model.init_session(task_id, learning_rate, training_classes[task_id])
-            lower_weights, upper_weights = weights_storage.return_weights()
-            model.assign_weights(list(range(no_heads)), lower_weights, upper_weights)
-            if len(x_coresets) > 0:
-                print('Training on coreset data...')
-                x_train_coreset, y_train_coreset = utils.merge_coresets(x_coresets, y_coresets)
-                bsize = x_train_coreset.shape[0] if (batch_size is None) else batch_size
-                _, _ = model.train(x_train_coreset, y_train_coreset, task_id,
-                                   lower_weights, upper_weights, no_epochs, bsize)
+            # Append accuracies to all_acc array
+            # if task_id == 0:
+            #     all_acc = np.array(acc)
+            # else:
+            #     all_acc = np.vstack([all_acc, acc])
+            # print(all_acc)
 
-            # Test-time: Calculate test accuracy
-            acc_interm = utils.get_scores_output_pred(model, x_testsets, y_testsets, test_classes,
-                                               task_idx=list(range(task_id+1)), multi_head=multi_head)
-            acc[:task_id+1] = acc_interm
+            result["test_metrics"] = acc
+            results.append(result)
 
-            model.close_session()
+        bsh.register_results(hypotheses, results)
+        bsh.prune_beams()
 
-        # Append accuracies to all_acc array
-        if task_id == 0:
-            all_acc = np.array(acc)
-        else:
-            all_acc = np.vstack([all_acc, acc])
-        print(all_acc)
+        pickle.dump(bsh, open(f"{path.rstrip('/')}/beam_history.pkl", "wb"))
 
-    return all_acc
+        hypotheses = bsh.get_new_hypotheses()
+        
+
+    return bsh  #all_acc
